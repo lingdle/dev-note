c65b734d95257a5797c7e6e47621c21f985f7d30
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2022-11-18 22:48:40 +0800

    Update upgrade-devops-tools.sh

--- upgrade-devops-tools.sh
+++ upgrade-devops-tools.sh
@@ -43,7 +43,7 @@ chsh -s $(which zsh)
 fi
 
 
-# 安装 nvm
+# 检查并安装 nvm
 if [ ! -x "$(command -v nvm)" ]; then
 curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
@@ -57,19 +57,20 @@ echo "$NVM_CONFIG" >> ~/.zshrc
 fi
 
 
-# 安装 jabba
+# 检查并安装 jabba
 if [ ! -x "$(command -v jabba)" ]; then
 curl -fsSL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 fi
 
-# 安装 mvn
+# 检查并安装 mvn
 if [ ! -x "$(command -v mvn)" ]; then
 sudo apt install maven -y
 mvn -v
 fi
 
+
+# 检查并安装 sublime
 if [ ! -x "$(command -v subl)" ]; then
-# 安装 sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -77,6 +78,8 @@ sudo apt update -y
 sudo apt install sublime-text -y
 fi
 
+
+# 检查并安装 jetbrains-toolbox-app
 JETBRAINS_TOOLBOX_HOME=/opt/jetbrains/jetbrains-toolbox/
 if [ ! -f "$JETBRAINS_TOOLBOX_HOME/jetbrains-toolbox" ]; then
 mkdir -p /opt/jetbrains/jetbrains-toolbox/
@@ -89,8 +92,7 @@ curl https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&
 
 ls /opt/jetbrains/jetbrains-toolbox*.tar.gz | xargs -i tar -zxvf {} -C $JETBRAINS_TOOLBOX_HOME --strip-components 1
 
-
-
+# TODO 设置环境变量
 fi
 
 
