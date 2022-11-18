efc50948c73b2cb41255d7b267f6cdb70ce7ef6f
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2022-11-18 22:24:39 +0800

    Create upgrade-devops-tools.sh

---
+++ upgrade-devops-tools.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# 安装 devops 偏好工具 
+
+set -xe
+# 剔除 windows 中的命令，防止 npm 等命令污染
+export PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
+
+# 启用并测试代理， 需 windows 主机已启用代理在7890端口
+WSL_HOST_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')
+export http_proxy=http://$WSL_HOST_IP:7890
+export https_proxy=http://$WSL_HOST_IP:7890
+curl -I https://raw.github.com
+
+# 检查并安装 git
+if [ ! -x "$(command -v git)" ]; then
+sudo apt install git -y
+fi
+
+# 检查并安装 xsel
+if [ ! -x "$(command -v xsel)" ]; then
+sudo apt install xsel -y
+fi
+
+# 检查并安装 jq
+if [ ! -x "$(command -v jq)" ]; then
+sudo apt install jq -y
+fi
+
+# 检查并安装 zsh
+if [ ! -x "$(command -v zsh)" ]; then
+sudo apt install zsh -y
+## 安装 oh-my-zsh : 如果安装后没有自动设置zsh为默认 terminal，则需要手动设置
+rm -rf ~/.oh-my-zsh
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+## 插件：命令联想提示
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+## 插件：命令错误检查
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+## 启用插件
+sed -i 's/plugins=(git)/plugins=( git zsh-autosuggestions zsh-syntax-highlighting )/g' ~/.zshrc
+## 切换系统默认 shell 为 zsh
+chsh -s $(which zsh)
+fi
+
+
+# 安装 nvm
+if [ ! -x "$(command -v nvm)" ]; then
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+NVM_CONFIG='# NVM configuration
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
+
+## 追加配置 ~/.zshrc
+echo "$NVM_CONFIG" >> ~/.zshrc
+fi
+
+
+# 安装 jabba
+if [ ! -x "$(command -v jabba)" ]; then
+curl -fsSL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+fi
+
+# 安装 mvn
+if [ ! -x "$(command -v mvn)" ]; then
+sudo apt install maven -y
+mvn -v
+fi
+
+if [ ! -x "$(command -v subl)" ]; then
+# 安装 sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update -y
+sudo apt install sublime-text -y
+fi
+
