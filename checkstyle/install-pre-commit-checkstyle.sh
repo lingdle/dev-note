41e7bea45874e6f48f117c3447114f4ae7357fe0
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2023-01-20 10:47:22 +0800

    Create install-pre-commit-checkstyle.sh

---
+++ checkstyle/install-pre-commit-checkstyle.sh
@@ -0,0 +1,87 @@
+#!/bin/bash -e
+readonly CURR_DIR=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+
+readonly checkstyleJarRepo='https://github.com/checkstyle/checkstyle/releases/download/checkstyle-9.3/checkstyle-9.3-all.jar'
+readonly checkstyleJarName='checkstyle-9.3-all.jar'
+readonly checkstyleJarFile="$CURR_DIR/$checkstyleJarName"
+
+#readonly checkstyleConfigRepo='https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml'
+readonly checkstyleConfigRepo='https://raw.githubusercontent.com/lingdle/dev-note/master/checkstyle/google-checkstyle-fix.xml'
+readonly checkstyleConfigName='google-checkstyle.xml'
+readonly checkstyleConfigFile="$CURR_DIR/$checkstyleConfigName"
+
+readonly preCommitSourceShellRepo="https://raw.githubusercontent.com/lingdle/dev-note/master/checkstyle/pre-commit-java-checkstyle.sh"
+readonly preCommitSourceShellName="pre-commit.sh"
+readonly preCommitSourceShellFile="$CURR_DIR/$preCommitSourceShellName"
+
+readonly preCommitGitShellFile="$CURR_DIR/../.git/hooks/pre-commit"
+
+function checkCheckstyleJar() {
+  echo "Check checkstyle jar ..."
+  if [ ! -f "$checkstyleJarFile" ]; then
+    echo "    checkstyle jar not find: $checkstyleJarFile"
+    echo "    download checkstyle jar..."
+    wget "$checkstyleJarRepo" -O "$checkstyleJarFile"
+    echo "    checkstyle jar readied:$checkstyleJarFile"
+  else
+    echo "    checkstyle jar already exist:$checkstyleJarFile"
+  fi
+}
+
+function checkCheckstyleConfig() {
+  echo "Check checkstyle config ..."
+  if [ ! -f "$checkstyleConfigFile" ]; then
+    echo "    checkstyle config not find: $checkstyleConfigFile"
+    echo "    download checkstyle config..."
+    wget "$checkstyleConfigRepo" -O "$checkstyleConfigFile"
+    echo "    checkstyle config readied:$checkstyleConfigFile"
+  else
+    echo "    checkstyle config already exist:$checkstyleJarFile"
+  fi
+}
+
+function checkPreCommitShell() {
+  echo "Check pre-commit shell ..."
+  if [ ! -f "$preCommitSourceShellFile" ]; then
+    echo "    pre-commit shell not find: $preCommitSourceShellFile"
+    echo "    download pre-commit shell..."
+    wget "$preCommitSourceShellRepo" -O "$preCommitSourceShellFile"
+    echo "    pre-commit shell readied:$preCommitSourceShellFile"
+  else
+    echo "    pre-commit shell already exist: $preCommitSourceShellFile"
+  fi
+}
+
+function linkGitPreCommit() {
+  echo "Link git pre-commit ..."
+  if [ ! -f "$preCommitGitShellFile" ]; then
+    echo "    pre-commit shell not find: $preCommitGitShellFile"
+    echo "    link $preCommitSourceShellFile to $preCommitGitShellFile"
+    ln -s "$preCommitSourceShellFile" "$preCommitGitShellFile"
+    ls -l "$preCommitGitShellFile"
+  else
+    echo "    pre-commit shell already exist: $preCommitGitShellFile"
+    ls -l "$preCommitGitShellFile"
+    echo "    maybe need copy shell $preCommitSourceShellName append to: $preCommitGitShellFile"
+    exit 1
+  fi
+}
+
+
+function testPreCheckstyle() {
+  echo "Test checkstyle at pre-commit ..."
+  echo "    git commit -m 'test checkstyle' -a "
+}
+
+echo "=============================="
+echo "Install checkstyle for pre-commit..."
+checkCheckstyleJar
+checkCheckstyleConfig
+checkPreCommitShell
+linkGitPreCommit
+testPreCheckstyle
+echo "=============================="
+#exit 1
