ca2f1a245118b83658d71751b0a8361303c9b49e
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2023-01-20 15:31:37 +0800

    Update pre-commit-java-checkstyle.sh

--- checkstyle/pre-commit-java-checkstyle.sh
+++ checkstyle/pre-commit-java-checkstyle.sh
@@ -1,29 +1,60 @@
 #!/bin/bash -e
+readonly CURR_DIR=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+
+readonly checkstyleHome="$CURR_DIR/../../checkstyle"
+
+readonly checkstyleJarName='checkstyle-9.3-all.jar'
+readonly checkstyleJarFile="$checkstyleHome/$checkstyleJarName"
+
+readonly checkstyleConfigName='google-checkstyle.xml'
+readonly checkstyleConfigFile="$checkstyleHome/$checkstyleConfigName"
+
+function checkCheckstyleJar() {
+  echo "Check checkstyle jar ..."
+  if [ ! -f "$checkstyleJarFile" ]; then
+    echo "    checkstyle jar not find: $checkstyleJarFile"
+    echo "    maybe need run ./install-pre-commit-checkstyle.sh download checkstyle jar..."
+    exit 1
+  else
+    echo "    checkstyle jar already exist:$checkstyleJarFile"
+  fi
+}
+
+function preCommitCheckstyle() {
+  echo "Find uncheck files..."
+  echo "    check file set:"
+  echo "    --------------------------"
+  files=()
+  #for file in $(git status --porcelain | awk '{print $2}' | grep -Ev '.jar$|.sh$'); do
+  #for file in $(git status --porcelain | awk '{print $2}' | grep -E '.java$|.properties$|.xml$'); do
+  #for file in $(git status --porcelain | awk '{print $2}' | grep -E '.java$|.properties$'); do
+  #for file in $(git status --porcelain | awk '{print $2}' | grep -E '.java$'); do
+  for file in $(git status --porcelain | awk '{print $2}'); do
+    if [ -f "$file" ]; then
+      echo "    $file"
+      files+=("$file")
+    fi
+  done
+
+  echo "    --------------------------"
+
+  if [[ ${#files[@]} -gt 0 ]]; then
+    #set -x
+    java -Duser.language=zh -jar "$checkstyleJarFile" -c "$checkstyleConfigFile" "${files[@]}"
+    RET_CODE=$?
+    echo "Errors $RET_CODE"
+    else
+      echo "uncheck files is empty."
+  fi
+}
+
 echo "=============================="
 echo "Checkstyle start..."
-echo "    check file set:"
-echo "    --------------------------"
-files=()
-#for file in $(git status --porcelain | awk '{print $2}' | grep -Ev '.jar$|.sh$'); do
-#for file in $(git status --porcelain | awk '{print $2}' | grep -E '.java$|.properties$|.xml$'); do
-#for file in $(git status --porcelain | awk '{print $2}' | grep -E '.java$|.properties$'); do
-#for file in $(git status --porcelain | awk '{print $2}' | grep -E '.java$'); do
-for file in $(git status --porcelain | awk '{print $2}'); do
-  if [ -f "$file" ]; then
-    echo "    $file"
-    files+=("$file")
-  fi
-done
-
-echo "    --------------------------"
-
-if [[ ${#files[@]} -gt 0 ]]; then
-  #set -x
-  java -Duser.language=zh -jar checkstyle/checkstyle-9.3-all.jar -c checkstyle/google-checkstyle.xml "${files[@]}"
-  RET_CODE=$?
-  echo "Errors $RET_CODE"
-fi
-
+checkCheckstyleJar
+preCommitCheckstyle
 echo "Checkstyle done..."
 echo "=============================="
 #exit 1
