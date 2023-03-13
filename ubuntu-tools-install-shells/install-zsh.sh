b7c38b3fdfa3481477a7f787e35c49867f8e06c8
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2023-03-13 20:03:59 +0800

    Update install-zsh.sh

--- ubuntu-tools-install-shells/install-zsh.sh
+++ ubuntu-tools-install-shells/install-zsh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # 检查并安装 zsh
-if [ ! -x "$(command -v zsh)" ]; then
-sudo apt install zsh -y
+[ ! -x "$(command -v zsh)" ] && (sudo apt install zsh -y)
+
+
 ## 安装 oh-my-zsh : 如果安装后没有自动设置zsh为默认 terminal，则需要手动设置
 rm -rf ~/.oh-my-zsh
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
@@ -10,8 +11,14 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 ## 插件：命令错误检查
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 ## 启用插件
-sed -i 's/plugins=(git)/plugins=( git zsh-autosuggestions zsh-syntax-highlighting )/g' ~/.zshrc
+sed -i 's/plugins=(.*)/plugins=( git zsh-autosuggestions zsh-syntax-highlighting )/g' ~/.zshrc
+
+## 启用 devops 预设
+[ -s ~/.devops-preset.sh ] && (curl --create-dirs -o  ~/.devops-preset.sh https://raw.githubusercontent.com/lingdle/dev-note/master/ubuntu-tools-install-shells/devops-preset.sh)
+
+[ -z "$(cat ~/.zshrc | grep '# config devops preset')" ] && (sed -i '/source .*/a\# config devops preset\nsource ~/.devops-preset1.sh' ~/.zshrc)
+
 ## 切换用户默认 shell 为 zsh
-chsh -s $(which zsh)
-fi
+chsh -s $(which zsh) #&
+[ "$SHELL" = "$(which zsh)" ] && (chsh -s $(which zsh))
 # 参考执行命令： curl -fsSL https://raw.githubusercontent.com/lingdle/dev-note/master/ubuntu-tools-install-shells/install-zsh.sh | bash
