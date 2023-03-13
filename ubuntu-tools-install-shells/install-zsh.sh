18f11d982c52bda24d776eaea6a7d7b7ffb75477
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2023-03-13 20:42:34 +0800

    Update install-zsh.sh

--- ubuntu-tools-install-shells/install-zsh.sh
+++ ubuntu-tools-install-shells/install-zsh.sh
@@ -21,5 +21,6 @@ fi
 
 ## 切换用户默认 shell 为 zsh
 [ "$SHELL" != "$(which zsh)" ] && (chsh -s $(which zsh))
+chsh -s $(which zsh)
 wait
 # 参考执行命令： curl -fsSL https://raw.githubusercontent.com/lingdle/dev-note/master/ubuntu-tools-install-shells/install-zsh.sh | bash
