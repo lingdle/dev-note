ab823bd47554ae006a95960ae2779465d4ed46b6
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2023-03-14 11:09:34 +0800

    Update install-java.sh

--- ubuntu-tools-install-shells/install-java.sh
+++ ubuntu-tools-install-shells/install-java.sh
@@ -6,7 +6,7 @@ if [ ! -x "$(command -v jabba)" ]; then
 curl -fsSL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 jabba install adopt-openj9@1.8.0-262
 jabba ls
-jabba use ~adopt-openj9@1.8.0-262
+jabba use adopt-openj9@1.8.0-262
 jabba current
 java -version
 fi
