17848f79e3d5b1b2f6e69a019455a23355678880
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2023-03-14 10:59:34 +0800

    Create install-java.sh

---
+++ ubuntu-tools-install-shells/install-java.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# install jabba for java
+
+# 安装 jabba 
+if [ ! -x "$(command -v jabba)" ]; then
+curl -fsSL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+jabba install adopt-openj9@1.8.0-262
+jabba ls
+jabba use ~adopt-openj9@1.8.0-262
+jabba current
+java -version
+fi
+
+
