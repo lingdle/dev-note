7f56db6afbe1f1371f20d4e1736df6893ab9b60b
Jiang.Bian <30922425+lingdle-hi@users.noreply.github.com>
2023-03-16 20:11:44 +0800

    Create install-nvm.sh

---
+++ ubuntu-tools-install-shells/install-nvm.sh
@@ -0,0 +1,12 @@
+# 检查并安装 nvm
+if [ ! -x "$(command -v nvm)" ]; then
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+#NVM_CONFIG='# NVM configuration
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
+
+## 追加配置 ~/.zshrc
+#echo "$NVM_CONFIG" >> ~/.zshrc
+fi
